08ddc54a1dd68adccf6ab945067499756b3fb262
Tosmim <tosmimforidmehtab@gmail.com>
2025-04-12 11:32:12 +0530

    Added recipe Tests and Apis

---
+++ app/recipe/urls.py
@@ -0,0 +1,9 @@
+from recipe import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("recipes", views.RecipeViewSet, basename="recipe")
+app_name = "recipe"
+
+urlpatterns = [path("", include(router.urls))]
